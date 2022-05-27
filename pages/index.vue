9909ea5df50857af9c80681614de5eabd7418d05
NeginSal <neginsaljooghi@gmail.com>
2022-05-27 16:56:52 +0430

    use AsynctAwait to fetch data

--- pages/index.vue
+++ pages/index.vue
@@ -23,20 +23,33 @@ export default {
     };
   },
 
-  asyncData({ $axios, error }) {
-    return $axios
-      .get("http://localhost:3000/events")
-      .then((response) => {
-        return {
-          events: response.data,
-        };
-      })
-      .catch((e) => {
-        error({
-          statusCode: 503,
-          message: "Unable to fetch events at this time . Please try again.",
-        });
+  // asyncData({ $axios, error }) {
+  //   return $axios
+  //     .get("http://localhost:3000/events")
+  //     .then((response) => {
+  //       return {
+  //         events: response.data,
+  //       };
+  //     })
+  //     .catch((e) => {
+  //       error({
+  //         statusCode: 503,
+  //         message: "Unable to fetch events at this time . Please try again.",
+  //       });
+  //     });
+  // },
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await $axios.get("http://localhost:3000/events");
+      return {
+        events: data,
+      };
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: "Unable to fetch events at this time . Please try again.",
       });
+    }
   },
 };
 </script>
